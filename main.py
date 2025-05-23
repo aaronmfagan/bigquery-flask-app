e263e16ca7e8996b049e132d74c5ed6542ee4f34
Aaron Fagan <aaronmfagan@gmail.com>
2025-05-23 08:57:04 -0700

    debug: change port to 8080

--- main.py
+++ main.py
@@ -40,6 +40,6 @@ def init_bigquery():
         return jsonify({"status": "error", "message": str(e)}), 400
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 8080))  # default to 8080
     print(f"Running on host=0.0.0.0, port={port}")
     app.run(host="0.0.0.0", port=port, debug=True)
